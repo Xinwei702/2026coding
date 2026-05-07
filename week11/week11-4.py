d4dacdf840d7f1b9d2900aea6b63eb0dcd73bc92
Xinwei702 <xinweiw963@gmail.com>
2026-05-07 16:15:35 +0800

    week11

---
+++ week11/week11-4.py
@@ -0,0 +1,14 @@
+# week11-4.py
+# LeetCode 700. Search in a Binary Search Tree
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        def helper(root, val):
+            if root == None:
+                return None
+            if val < root.val:
+                return helper(root.left, val)
+            if val > root.val:
+                return helper(root.right, val)
+            if val == root.val:
+                return root
+        return helper(root, val)
